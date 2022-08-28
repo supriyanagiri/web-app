c73c96275cc12cd114834859315785699073acbf
sudhakargit <venkatasudhakar33@gmail.com>
2022-08-28 14:45:56 +0530

    Update MainController.java
    updates title

--- src/main/java/com/demo/app/MainController.java
+++ src/main/java/com/demo/app/MainController.java
@@ -9,8 +9,8 @@ public class MainController {
 	
 	@GetMapping("/")
     public String index(final Model model) {
-		model.addAttribute("title", "Nagiri Supriya - Devops Engineer");
-		model.addAttribute("msg", "Over 2+ years of experience with DevOps and System Engineering in Multi Cloud environments such as AWS, GCP, AZURE for managing servers hosted on them.");
+		model.addAttribute("title", "Nagiri Supriya - Devops and Cloud Engineer");
+		model.addAttribute("msg", "Over 2.6 years of experience with DevOps and System Engineering in Multi Cloud environments such as AWS, GCP, AZURE for managing servers hosted on them.");
 		return "index";
   }
 
